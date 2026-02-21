54aa01eed7a13c6a7a0f2146a3ce60e2cce55c74
Tushar Konala <tusharkonala284@gmail.com>
2026-02-21 13:54:51 +0530

    Implemented the main dashboard grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,38 @@
-import "./App.css";
-import Avatar from "./components/student-dashboard/avatar";
+import { Grid, GridItem } from "@chakra-ui/react";
+import Avatar from "./components/student-dashboard/Avatar";
+import ProfileCard from "./components/student-dashboard/ProfileCard";
+import StreakCalendar from "./components/student-dashboard/StreakCalendar";
+import Notifications from "./components/student-dashboard/Notifications";
 
-function App() {
-  return <Avatar />;
-}
+const App = () => {
+  return (
+    <Grid
+      templateColumns={{ base: "1fr", md: "repeat(2, 1fr)" }}
+      templateRows="auto"
+      gap={6}
+      p={6}
+      bg="#F7FAFC"
+      minH="100vh"
+    >
+      {/* Row 1 */}
+      <GridItem>
+        <Avatar />
+      </GridItem>
+
+      <GridItem>
+        <ProfileCard />
+      </GridItem>
+
+      {/* Row 2 */}
+      <GridItem>
+        <StreakCalendar />
+      </GridItem>
+
+      <GridItem>
+        <Notifications />
+      </GridItem>
+    </Grid>
+  );
+};
 
 export default App;
